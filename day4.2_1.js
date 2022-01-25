dc3f0faf6107f825a30cb5ba3ff14e40f6d21929
vielennyy <76629480+vielennyy@users.noreply.github.com>
2022-01-26 00:15:13 +0200

    Add files via upload

---
+++ day4.2_1.js
@@ -0,0 +1,70 @@
+function next_birthday(horizont){
+    //array whith objects (employees)
+    let employees = [{person:'Lina Marchenko', date:12, month:1, year:2000},
+                {person:'Ivan Pavlenko', date:4, month:2, year:1997},
+                {person:'Marina Kornienko', date:19, month:1, year:2003},
+                {person:'Ruslan Kuzmenko', date:27, month:3, year:2002},
+                {person:'Volodymyr Osaulenko', date:16, month:2, year:1900}];
+    
+    let current_year = 2022;
+    let current_month = 0;
+    let array_month_keys = [
+        `January ${current_year}`,
+        `February ${current_year}`,
+        `March ${current_year}`,
+        `April ${current_year}`,
+        `May ${current_year}`,
+        `June ${current_year}`,
+        `July ${current_year}`,
+        `August ${current_year}`,
+        `September ${current_year}`,
+        `October ${current_year}`,
+        `November ${current_year}`,
+        `December ${current_year}`
+    ];
+   
+    let employees_map = new Map();
+    let temp = [];
+    //fill arrays with employees 
+    // for (var j = current_month; j <= horizont; j++){
+        for (var i = 0; i < employees.length; i ++){
+            temp = [];
+            if (!(employees_map.has(employees[i].month))){
+                temp.push(employees[i]);
+                employees_map.set(employees[i].month, employees[i]);
+            }
+            else {
+                temp.push(employees_map.get(employees[i].month));
+                temp.push(employees[i]);
+                temp.sort(function(a, b) {
+                    return a.date - b.date;
+                });
+                employees_map.set(employees[i].month, temp);
+            }
+        } 
+        for (var i = current_month; i <= horizont; i++){
+            console.log(`${array_month_keys[i]}`);
+            var m = employees_map.get(employees[i].month);
+            for (var j = 0; j < m.length; j++)
+            {
+                var o = m[j];
+                var age = current_year - o.year; 
+                if (age % 10 == 1)
+                    console.log(`(${o.date}) - ${o.person} (${age} год)`);
+                else if (age % 10 == 2 || age % 10 == 3 || age % 10 == 4)
+                    console.log(`(${o.date}) - ${o.person} (${age} года)`);
+                else 
+                    console.log(`(${o.date}) - ${o.person} (${age} лет)`);
+            }
+            // let str = Object.values(employees_map.get(employees[i].month));
+            // console.log(str);
+
+            // console.log(JSON.stringify(employees_map.get(employees[i].month)));
+        }        
+    // console.log(employees_map);
+
+
+    
+}
+
+next_birthday(1);
--- day5.1.js
+++ day5.1.js
@@ -14,19 +14,56 @@ console.log(horizont) ;
 //     month: number;
 //     year: number;
 //   };
+var current_year = 2022;
+let array_month_keys = [
+    `January ${current_year}`,
+    `February ${current_year}`,
+    `March ${current_year}`,
+    `April ${current_year}`,
+    `May ${current_year}`,
+    `June ${current_year}`,
+    `July ${current_year}`,
+    `August ${current_year}`,
+    `September ${current_year}`,
+    `October ${current_year}`,
+    `November ${current_year}`,
+    `December ${current_year}`
+];
+
 
 const csv = require('csv-parser');
 var fs = require("fs");
-const results = [];
+var results = [];
 
 fs.createReadStream(path)
     .pipe(csv({}))
     .on('data', (data) => results.push(data))
-    .on('end', () => {
+    .on('end', () => 
+    {
+        let map = new Map();
         console.log(results);
-    });
-
-
-
-
+        var temp = [];
+        for (i = 0; i < results.length; i++)
+        {
+            var el_res = results[i];
+            var bday_arr = el_res.birthday.split('/');
+            
+            if (map.has(bday_arr[0]))
+            {
+                temp = [];
+                temp.push(map.get(bday_arr[0]));
+                temp.push(el_res);
+                temp.sort(function(a, b) {
+                    return a.date - b.date;
+                });
+                map.set(bday_arr[0], temp);   
+            }
+            else 
+            {
+                map.set(bday_arr[0], el_res)
+            }
+        }
+        console.log(map);
+    }
+);
 
